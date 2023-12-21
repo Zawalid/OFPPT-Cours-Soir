133656f14c1cab9b56957e638d7956208423b073
Zaakaria <zikolbyad1@gmail.com>
2023-12-21 20:45:33 +0100

    'Laravel'

---
+++ database/migrations/2023_12_21_150432_create_activites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('activites', function (Blueprint $table) {
+            $table->id();
+            $table->string('Titre');
+            $table->date('Date_Debut');
+            $table->date('Date_Fin');
+            $table->string('Lieu');
+            $table->string('Thumbnail');
+            $table->unsignedBigInteger('Annee_Formation_id');
+            $table->foreign('Annee_Formation_id')->references('id')->on('Annee_Formation')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activites');
+    }
+};
---
+++ database/migrations/2023_12_21_150503_create_filiers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('filiers', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nom');
+            $table->text('Description');
+            $table->integer('Number_Stagiaires');
+            $table->integer('Max_Stagiaires');
+            $table->unsignedBigInteger('Annee_Formation_id');
+            $table->foreign('Annee_Formation_id')->references('id')->on('Annee_Formation')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filiers');
+    }
+};
